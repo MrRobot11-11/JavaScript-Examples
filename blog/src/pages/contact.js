214848f2f4703a185750dce06a98864e10afdefa
Steve Lantz <lantzs@gmatc.matc.edu>
2022-03-16 18:41:09 -0500

    Added Comments

--- blog/src/pages/contact.js
+++ blog/src/pages/contact.js
@@ -5,11 +5,11 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Contact = ({ data }) => {
-    const { name, company, address } = data.site.siteMetadata.contact;
-    return (
+const Contact = ({ data }) => { //Destructure data to get name, company, address
+    const { name, company, address } = data.site.siteMetadata.contact; //structure of the query-> where the query comes from
+    return (  //Grouping html that we're going to return
         <Layout>
-            <Seo title="Home" />
+            <Seo title="Contact" />
             <h1>Contact Us</h1>
             <p>Please send all inquiries to: </p>
             <div>{company}</div>
@@ -30,9 +30,10 @@ const Contact = ({ data }) => {
         )
 }
 
-export default Contact
-
-export const query = graphql`
+export default Contact  //Have to export so build engine can access it
+                                
+/*Running query through function graphgl() */
+export const query = graphql` 
     query {
         site {
             siteMetadata {
