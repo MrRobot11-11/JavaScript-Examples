aa643f4be11f09759702eb2fee5442a4abe6ca48
Steve Lantz <lantzs@gmatc.matc.edu>
2022-04-07 21:44:14 -0500

    Add button

--- blog/src/components/Button/BaseButton.js
+++ blog/src/components/Button/BaseButton.js
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types'
 import { Button } from 'rebass'
 import styled from 'styled-components'
 
-const Reset = styled(Button)`
+/* Create a styled component named Reset that 
+ * extends the Rebass Button and defines button reset styles.
+ */
+
+const Reset = styled(Button)` /* When the component is not an official html component, 
+                                this is the syntax that we use to pass it in 
+                              */
     background: none;
     box-shadow: none;
     font-weight: normal;
     border-radius: 0;
     cursor: pointer;
 `
-
-export const BaseButton = ({ childre, ...rest}) => 
+/* Create a component named BaseButton that wraps 
+ * the Reset component.
+ */
+/* Destructure the children prop and stuff the remaining 
+  (Rebass) props into a rest parameter named rest. Pass the 
+  rest parameter to the Reset button, which will allow consumers 
+  to apply Rebass props to the underlying Rebass button.
+ */
+export const BaseButton = ({ children, ...rest}) => 
 <Reset {...rest}>{children}</Reset>
 
+/* Define a required propType for the children prop.
+ */
 BaseButton.propTypes = {
     children: PropTypes.node.isRequired
 }
---
+++ blog/src/components/Button/IconButton.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { BaseButton } from './BaseButton'
+
+/* Create a styled component named StyledButton that extends BaseButton. 
+   We’ll use this component to inject theme styles, but for now we’ll 
+   just leave it empty. 
+ */
+const StyledButton = styled(BaseButton)`
+//Inject them styles here later
+`
+/*  Create a styled component named IconButton that wraps StyledButton. 
+ */
+/*  Destructure the icon prop and stuff the remaining (Rebass) props 
+    into a rest parameter named rest. 
+ */
+/*  Clone the icon, passing along the rest parameter to the cloneElement method.
+ */
+/*  Return the StyledButton. Set the Rebase as prop to the cloned icon’s type 
+   (which is a styled component), pass along the rest parameter, then set the className. 
+ */
+/*  Note: we’re setting the className in order to reapply any classes that may have been 
+   attached to the props as a result extending and cloning; we will not be explicitly 
+   setting the className prop when we use the component. 
+*/
+/* Note: As a styled component, IconButton is followed with backticks, though in 
+  this case there are no styles, just a comment 
+*/
+export const IconButton = styled (({ icon, ...rest }) => {
+    let clone = React.cloneElement(icon, rest)
+    return <StyledButton as={clone.type} {...rest} className={rest.className} />
+})`
+    //We could add styles here, but it is not necessary.
+    //The main reason this is a styled component is to wrap
+    the styled icon passed in as a prop.
+`
+/* Define a defaultProp that sets the Rebass size prop to 24 
+*/
+IconButton.defaultProps = {
+    size: 24
+}
+
+/* Define a required propType for the icon prop. 
+*/
+IconButton.propTypes = {
+    icon: PropTypes.node.isRequired
+}
